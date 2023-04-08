0adef37533305530c4a0a5154f42da79112dc909
kyw0716 <kyw0716@naver.com>
2023-04-08 21:10:02 +0900

    fix: 스켈레톤 위치 맞지 않는 오류 수정

--- src/components/MovieList/index.ts
+++ src/components/MovieList/index.ts
@@ -28,6 +28,8 @@ export class MovieList {
     page: 1,
   };
 
+  #observer: IntersectionObserver | undefined;
+
   constructor($target: Element) {
     this.#$target = $target;
 
@@ -40,11 +42,9 @@ export class MovieList {
     try {
       const { results, total_pages } = await getPopularMovies(this.#state.page);
 
-      this.render(results, total_pages);
+      this.#observer = new IntersectionObserver(this.fetchNextPage.bind(this));
 
-      new IntersectionObserver(this.fetchNextPage.bind(this)).observe(
-        $(".btn")
-      );
+      this.render(results, total_pages);
     } catch {
       deleteSkeletonContainer();
     }
@@ -59,7 +59,7 @@ export class MovieList {
       `
     );
 
-    if (this.#state.page === total_pages) this.hideMore();
+    this.#state.page === total_pages ? this.hideMore() : this.showMore();
 
     if (this.#state.page === 1 && movieList.length === 0) {
       const subTitle = $(".sub-title");
@@ -68,16 +68,15 @@ export class MovieList {
     }
   }
 
-  async changeShowTarget(state: showType, searchKeyword?: string) {
+  async changeShowTarget(showState: showType, searchKeyword?: string) {
     this.#$target.innerHTML = ``;
     this.#state = {
       ...this.#state,
-      showState: state,
+      showState,
       page: 1,
       searchKeyword: searchKeyword ?? "",
     };
 
-    this.showMore();
     showSkeletonContainer();
 
     const { results, total_pages } =
@@ -91,7 +90,13 @@ export class MovieList {
   }
 
   async fetchNextPage() {
+    const itemList = $(".item-list");
+
+    if (!(itemList instanceof HTMLElement)) return;
+
     this.#state.page += 1;
+
+    itemList.style.marginBottom = "48px";
     showSkeletonContainer();
 
     try {
@@ -100,17 +105,22 @@ export class MovieList {
           ? await getPopularMovies(this.#state.page)
           : await getSearchResult(this.#state.page, this.#state.searchKeyword);
 
+      if (this.#state.page === total_pages) this.hideMore();
+
       this.render(results, total_pages);
-    } catch {
+    } finally {
       deleteSkeletonContainer();
+      itemList.style.marginBottom = "0";
     }
   }
 
   hideMore() {
-    $(".btn").setAttribute("hidden", "");
+    console.log("멈춤");
+    this.#observer?.unobserve($(".btn"));
   }
 
   showMore() {
-    $(".btn").removeAttribute("hidden");
+    console.log("시작", $(".btn"));
+    this.#observer?.observe($(".btn"));
   }
 }
