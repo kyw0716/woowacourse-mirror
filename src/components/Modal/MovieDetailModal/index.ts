1aa7096cf7771eb86fc4902a119c215e3d270e52
kyw0716 <kyw0716@naver.com>
2023-04-09 16:54:23 +0900

    refactor: 별점 선택 방식 마우스 호버 => 클릭 후 드래그로 변경

--- src/components/Modal/MovieDetailModal/index.ts
+++ src/components/Modal/MovieDetailModal/index.ts
@@ -6,46 +6,65 @@ import {
   getStarRateFromStorage,
   setStarRateToStorage,
 } from "../../../utils/storage";
-import { $ } from "../../../utils/selector";
-import { renderStars } from "./StarSelect";
+import { $, $$ } from "../../../utils/selector";
 import { getImageContainerTemplate } from "./Image";
 import { getDescriptionTemplate } from "./Description";
 import { convertMovieDetail, getMovieInfoById } from "../../../utils/api";
+
+const STAR_RATE_STRING = [
+  "나의 점수는?",
+  "최악이예요",
+  "별로예요",
+  "보통이예요",
+  "재미있어요",
+  "명작이예요",
+];
 
 export class MovieDetailModal {
   #$target;
 
   constructor($target: Element) {
     this.#$target = $target;
-
-    this.bindEvent();
   }
 
-  bindEvent() {
-    $(".modal-content").addEventListener("mouseover", (event: Event) => {
-      if (
-        !(
-          event.target instanceof HTMLImageElement ||
-          event.target instanceof HTMLSpanElement
-        )
-      )
-        return;
-      if (
-        !(
-          event.target.className === "star-rate-select-img" ||
-          event.target.className === "select-zero-rate"
-        )
-      )
-        return;
+  initStarRate() {
+    const starContainer = $$(".star");
+    const starInput = $(".star-rating");
+    const starRateNum = $(".star-rate-num");
+    const starRateDesc = $(".star-rate-desc");
 
-      const movieId = Number(event.target.dataset.movieId);
-      const starRate = Number(event.target.dataset.starRate);
+    if (!(starInput instanceof HTMLInputElement)) return;
+    if (!(starRateNum instanceof HTMLSpanElement)) return;
+    if (!(starRateDesc instanceof HTMLSpanElement)) return;
 
-      if (getStarRateFromStorage(movieId) !== starRate) {
-        renderStars(movieId, starRate);
-        setStarRateToStorage(movieId, starRate);
-      }
+    this.fillStar(starContainer, starInput);
+    this.bindStarRateEvent(starInput, starContainer, starRateNum, starRateDesc);
+  }
+
+  bindStarRateEvent(
+    starInput: HTMLInputElement,
+    starContainer: NodeListOf<Element>,
+    starRateNum: HTMLSpanElement,
+    starRateDesc: HTMLSpanElement
+  ) {
+    starInput.addEventListener("input", () => {
+      const movieId = starInput.dataset.movieId;
+
+      this.fillStar(starContainer, starInput);
+
+      starRateNum.innerHTML = `${Number(starInput.value) * 2}점`;
+      starRateDesc.innerHTML = STAR_RATE_STRING[Number(starInput.value)];
+
+      setStarRateToStorage(Number(movieId), Number(starInput.value));
     });
+  }
+
+  fillStar(starContainer: NodeListOf<Element>, starInput: HTMLInputElement) {
+    for (let i = 0; i <= starContainer.length; i++) {
+      if (i + 1 <= Number(starInput.value))
+        starContainer[i].classList.add("yellow");
+      else starContainer[i]?.classList.remove("yellow");
+    }
   }
 
   async render(movieId: number) {
@@ -59,6 +78,8 @@ export class MovieDetailModal {
       movieDetail,
       getStarRateFromStorage(movieId)
     );
+
+    this.initStarRate();
   }
 
   getMovieDetailTemplate(movie: MovieDetail, starRate: number) {
